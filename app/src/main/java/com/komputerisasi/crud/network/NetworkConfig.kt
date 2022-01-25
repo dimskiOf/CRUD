27527e1f67dbe37c578c9e492bad2e4a44468545
rezdnov <rezky.dimas1@gmail.com>
2022-01-26 06:46:00 +0700

    Dims

--- app/src/main/java/com/komputerisasi/crud/network/NetworkConfig.kt
+++ app/src/main/java/com/komputerisasi/crud/network/NetworkConfig.kt
@@ -1,8 +1,14 @@
 package com.komputerisasi.crud.network
 
+
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.komputerisasi.crud.konfigurasi.SettingContract
 import com.komputerisasi.crud.model.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.jetbrains.anko.db.*
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -25,21 +31,52 @@ object NetworkConfig {
         return okHttpClient
     }
 
+
+
     //Retrofit
-    fun getRetrofit(): Retrofit {
+    fun getRetrofit(setter:String): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(setter)
             .client(getInterceptor())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-
-    fun getService() = getRetrofit().create(StaffService::class.java)
+    fun getService() = getRetrofit(null.toString()).create(StaffService::class.java)
 
 }
 
 
+class DatabaseHelper(context: Context) : ManagedSQLiteOpenHelper(context, "database_settings.db", null, 1) {
+
+    companion object {
+        private var instance: DatabaseHelper? = null
+
+        fun getInstance(context: Context): DatabaseHelper {
+            if (instance == null) {
+                instance = DatabaseHelper(context)
+            }
+            return instance as DatabaseHelper
+        }
+
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        //Buat tabel pada database
+        var dt = db?.rawQuery("select * from table_settings where name_setting = 'settingurl'",null)
+        if(dt!!.moveToNext()){
+            NetworkConfig.getRetrofit(dt.getString(2))
+        }
+        NetworkConfig.getRetrofit("http://localhost/")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.dropTable(SettingContract.TABLE_SETTING, true)
+    }
+}
+
+val Context.database: DatabaseHelper
+get() = DatabaseHelper(applicationContext)
 
 interface StaffService{
     //Fungsi Login Data
